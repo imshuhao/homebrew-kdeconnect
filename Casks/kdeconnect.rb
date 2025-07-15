281c9310c5fb947843349226cfd14414bd489a99
Shuhao Dong <me@shuhao.ca>
2025-07-14 23:37:57 -0400

    Update KDE Connect cask: set version to 0, replace placeholder SHA256 values with actual checksums, and refine SHA256 update logic in workflows.

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -2,7 +2,7 @@ cask "kdeconnect" do
   name "KDE Connect"
   desc "Connect your phone to your computer"
   homepage "https://kdeconnect.kde.org/"
-  version "5218"
+  version "0"
 
   livecheck do
     url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-arm64/"
@@ -13,10 +13,10 @@ cask "kdeconnect" do
   on_macos do
     if Hardware::CPU.arm?
       url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-arm64/kdeconnect-kde-master-5218-macos-clang-arm64.dmg"
-      sha256 "placeholder"
+      sha256 "35eef0facd814f3503de43e935ae650f1616a0c0d7b39b7821ad1f1c83373063"
     else
       url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-x86_64/kdeconnect-kde-master-5218-macos-clang-x86_64.dmg"
-      sha256 "placeholder"
+      sha256 "35eef0facd814f3503de43e935ae650f1616a0c0d7b39b7821ad1f1c83373063"
     end
   end
 
