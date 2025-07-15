8dfc6d586eb9806d4013b2c4ce0971cf41594a92
Shuhao Dong <me@shuhao.ca>
2025-07-14 21:01:30 -0400

    revision

--- Formula/kdeconnect.rb
+++ Formula/kdeconnect.rb
@@ -1,34 +1,33 @@
 class Kdeconnect < Formula
-  desc "KDE Connect for macOS - connect your phone to your computer"
+  desc "Connect your phone to your computer"
   homepage "https://kdeconnect.kde.org/"
-  version "0.0.0"  # Version is meaningless for nightlies
+  version "0.0.0"
   license "GPL-2.0-or-later"
-  revision 1
-  
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-arm64/kdeconnect-kde-master-latest-macos-clang-arm64.dmg"
-      sha256 ""
-    else
-      url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-x86_64/kdeconnect-kde-master-latest-macos-clang-x86_64.dmg"
-      sha256 ""
-    end
-  end
 
   livecheck do
     skip "No version information available"
   end
 
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-arm64/kdeconnect-kde-master-latest-macos-clang-arm64.dmg"
+    else
+      url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-x86_64/kdeconnect-kde-master-latest-macos-clang-x86_64.dmg"
+    end
+  end
+
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
   def install
     # Extract the .app directory from the DMG and install to prefix
     mount_point = "/Volumes/KDE Connect"
-    
+
     # Find the DMG file
     dmg_file = Dir["#{staged_path}/*.dmg"].first
-    
+
     # Mount the DMG
     system "/usr/bin/hdiutil", "attach", "-nobrowse", "-readonly", "-mountpoint", mount_point, dmg_file
-    
+
     begin
       # Copy the app to the prefix
       prefix.install Pathname.glob("#{mount_point}/KDE Connect.app").first
@@ -46,5 +45,9 @@ class Kdeconnect < Formula
       You can link it into your Applications folder with:
         ln -sf "#{prefix}/KDE Connect.app" "/Applications/KDE Connect.app"
     EOS
+  end
+
+  test do
+    system "/usr/bin/false"
   end
 end
